2f2490140cb4f7d80e042654b2ce35b3b02e4084
Thorsten Jordan <thjordan@users.sf.net>
2004-02-01 21:50:03 +0000

    small test changes

--- dangerdeep/src/global_data.cpp
+++ dangerdeep/src/global_data.cpp
@@ -48,7 +48,7 @@ image *titlebackgrimg, *periscope, *threesubsimg, *damage_screen_background,
 void init_global_data(void)
 {
 	skyhemisphere = new model(get_model_dir() + "skyhemisphere.3ds", false);
-	water = new texture(get_texture_dir() + "water.png", GL_LINEAR_MIPMAP_LINEAR);
+	water = new texture(get_texture_dir() + "water.png", GL_LINEAR_MIPMAP_LINEAR, GL_CLAMP);
 	the_moon = new texture(get_texture_dir() + "themoon.png", GL_LINEAR);
 	the_sun = new texture(get_texture_dir() + "thesun.png", GL_LINEAR);
 	background = new texture(get_texture_dir() + "background.png", GL_LINEAR);
