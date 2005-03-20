6aab0e6efae383c1800e4fbe889440bdb4bb3560
Thorsten Jordan <thjordan@users.sf.net>
2005-03-20 11:27:22 +0000

    included tdc daylight graphics in code

--- dangerdeep/src/sub_tdc_display.cpp
+++ dangerdeep/src/sub_tdc_display.cpp
@@ -14,51 +14,106 @@
 using namespace std;
 
 
+sub_tdc_display::rotat_tex::rotat_tex() : tex(0), left(0), top(0), centerx(0), centery(0)
+{
+}
+
+sub_tdc_display::rotat_tex::~rotat_tex()
+{
+	delete tex;
+}
+
+void sub_tdc_display::rotat_tex::set(const char* filename, int left_, int top_, int centerx_, int centery_)
+{
+	delete tex;	// paranoia
+	tex = new texture(get_image_dir() + filename);
+	left = left_;
+	top = top_;
+	centerx = centerx_;
+	centery = centery_;
+}
+
+void sub_tdc_display::rotat_tex::draw(double angle) const
+{
+	// fixme: maybe rotate around pixel center (x/y + 0.5)
+	tex->draw_rot(centerx, centery, angle, centerx - left, centery - top);
+}
+
+
+sub_tdc_display::scheme::scheme()
+{
+	memset(this, 0, sizeof(*this));
+}
+
+
+
+sub_tdc_display::scheme::~scheme()
+{
+	delete background;
+	for (unsigned i = 0; i < 6; ++i) {
+		delete tubelight[i];
+		delete tubeswitch[i];
+	}
+	delete firebutton;
+	delete automode[0];
+	delete automode[1];
+	delete firesolutionquality;
+}
+
+
 
 sub_tdc_display::sub_tdc_display(user_interface& ui_) : user_display(ui_)
 {
-	background_normallight = new image(get_image_dir() + "TDC_daylight_base.png", true);
-//	background_nightlight = new image(get_image_dir() + "TDC_redlight_base.png", true);
+	selected_tube = 0;
+	selected_mode = 0;
 
-	tube_textures_daylight.resize(6);
-//	tube_textures_nightlight.resize(6);
+	normallight.background = new image(get_image_dir() + "TDC_daylight_base.png", true);
+//	nightlight.background = new image(get_image_dir() + "TDC_redlight_base.png", true);
+
+	normallight.clockbig.set("TDC_daylight_clockbigptr.png", 921, 124, 930, 134);
+//	nightlight.clockbig.set("TDC_redlight_clockbigptr.png", 921, 124, 930, 134);
+	normallight.clocksml.set("TDC_daylight_clocksmlptr.png", 921, 124, 930, 134);
+//	nightlight.clocksml.set("TDC_redlight_clocksmlptr.png", 921, 124, 930, 134);
+	normallight.targetcourse.set("TDC_daylight_targetcourse.png", 567, 365, 585, 451);
+//	nightlight.targetcourse.set("TDC_redlight_targetcourse.png", 567, 365, 585, 451);
+	normallight.targetrange.set("TDC_daylight_targetrange.png", 755, 231, 774, 317);
+//	nightlight.targetrange.set("TDC_redlight_targetrange.png", 755, 231, 774, 317);
+	normallight.targetspeed.set("TDC_daylight_targetspeed.png", 568, 101, 586, 187);
+//	nightlight.targetspeed.set("TDC_redlight_targetspeed.png", 568, 101, 586, 187);
+	normallight.spreadangle.set("TDC_daylight_spreadangle.png", 339, 102, 358, 188);
+//	nightlight.spreadangle.set("TDC_redlight_spreadangle.png", 339, 102, 358, 188);
+	normallight.targetpos.set("TDC_daylight_targetposition.png", 102, 109, 128, 188);
+//	nightlight.targetpos.set("TDC_redlight_targetposition.png", 102, 109, 128, 188);
+	normallight.gyro_a.set("TDC_daylight_gyroa.png", 66, 389, 127, 450);
+//	nightlight.gyro_a.set("TDC_redlight_gyroa.png", 66, 389, 127, 450);
+	normallight.gyro_v.set("TDC_daylight_gyrov.png", 282, 389, 345, 450);
+//	nightlight.gyro_v.set("TDC_redlight_gyrov.png", 282, 389, 345, 450);
+	normallight.torpspeed.set("TDC_daylight_torpspeed.png", 512, 116, 585, 188);
+//	nightlight.torpspeed.set("TDC_redlight_torpspeed.png", 512, 116, 585, 188);
+
 	for (unsigned i = 0; i < 6; ++i) {
 		ostringstream osn;
 		osn << (i+1);
-		tube_textures_daylight[i] = new texture(get_image_dir() + "TDC_daylight_tube" + osn.str() + ".png");
-//		tube_textures_nightlight[i] = new texture(get_image_dir() + "TDC_redlight_tube" + osn.str() + ".png");
+		normallight.tubelight[i] = new texture(get_image_dir() + "TDC_daylight_tube" + osn.str() + ".png");
+//		nightlight.tubelight[i] = new texture(get_image_dir() + "TDC_redlight_tube" + osn.str() + ".png");
+		normallight.tubeswitch[i] = new texture(get_image_dir() + "TDC_daylight_switchtube" + osn.str() + ".png");
+//		nightlight.tubeswitch[i] = new texture(get_image_dir() + "TDC_redlight_switchtube" + osn.str() + ".png");
 	}
 
-	clock_big_pointer_daylight = new texture(get_image_dir() + "TDC_daylight_clockbigptr.png");
-//	clock_big_pointer_nightlight = new texture(get_image_dir() + "TDC_redlight_clockbigptr.png");
-	clock_small_pointer_daylight = new texture(get_image_dir() + "TDC_daylight_clocksmlptr.png");
-//	clock_small_pointer_nightlight = new texture(get_image_dir() + "TDC_redlight_clocksmlptr.png");
-	targetpos_ptr_daylight = new texture(get_image_dir() + "TDC_daylight_targetposition.png");
-//	targetpos_ptr_nightlight = new texture(get_image_dir() + "TDC_redlight_targetposition.png");
-	targetcourse_ptr_daylight = new texture(get_image_dir() + "TDC_daylight_targetcourse.png");
-//	targetcourse_ptr_nightlight = new texture(get_image_dir() + "TDC_redlight_targetcourse.png");
+	normallight.firebutton = new texture(get_image_dir() + "TDC_daylight_firebutton.png");
+//	nightlight.firebutton = new texture(get_image_dir() + "TDC_redlight_firebutton.png");
+	normallight.automode[0] = new texture(get_image_dir() + "TDC_daylight_autoswitchon.png");
+//	nightlight.automode[0] = new texture(get_image_dir() + "TDC_redlight_autoswitchon.png");
+	normallight.automode[1] = new texture(get_image_dir() + "TDC_daylight_autoswitchoff.png");
+//	nightlight.automode[1] = new texture(get_image_dir() + "TDC_redlight_autoswitchoff.png");
+	normallight.firesolutionquality = new texture(get_image_dir() + "TDC_daylight_firesolutionquality.png");
+//	nightlight.firesolutionquality = new texture(get_image_dir() + "TDC_redlight_firesolutionquality.png");
 }
 
 
 
 sub_tdc_display::~sub_tdc_display()
 {
-	delete background_normallight;
-//	delete background_nightlight;
-
-	for (unsigned i = 0; i < 6; ++i) {
-		delete tube_textures_daylight[i];
-//		delete tube_textures_redlight[i];
-	}
-
-	delete clock_big_pointer_daylight;
-//	delete clock_big_pointer_nightlight;
-	delete clock_small_pointer_daylight;
-//	delete clock_small_pointer_nightlight;
-	delete targetpos_ptr_daylight;
-//	delete targetpos_ptr_nightlight;
-	delete targetcourse_ptr_daylight;
-//	delete targetcourse_ptr_nightlight;
 }
 
 
@@ -85,60 +140,68 @@ void sub_tdc_display::display(class game& gm) const
 	sys().prepare_2d_drawing();
 	glColor3f(1,1,1);
 
-	// draw background
+	// determine time of day
 	bool is_day = true;//gm.is_day_mode();
-	if (is_day) {
-		background_normallight->draw(0, 0);
-	} else {
-		background_nightlight->draw(0, 0);
-	}
+	const scheme& s = (is_day) ? normallight : nightlight;
 
-	// draw clock pointers
-/*
+	// draw background pointers/dials: gyros, firesolutionquality
+	s.gyro_a.draw(33/*fixme*/);
+	s.gyro_v.draw(33/*fixme*/);
+	float quality = 0.333f; // per cent, fixme
+	s.firesolutionquality->draw(926, 50+int(288*quality+0.5f));
+
+	// draw background
+	s.background->draw(0, 0);
+
+	// clock (torpedo run time), fixme: use real time as test
 	double t = gm.get_time();
 	double hourang = 360.0*myfrac(t / (86400/2));
 	double minuteang = 360*myfrac(t / 3600);
-	clock_hours_pointer->draw_rot(946, 294, hourang);
-	clock_minutes_pointer->draw_rot(946, 294, minuteang);
-*/
+	s.clocksml.draw(hourang);
+	s.clockbig.draw(minuteang);
 
+	// torpedo speed (depends on selected tube!), fixme
+	// 512,116, 585,188
+
+	// target values (influenced by quality!)
 	// get pointer to target and values
 	sea_object* target = ui.get_target();
-	double tgtcourse = 0, tgtpos = 0;
+	double tgtcourse = 0, tgtpos = 0, tgtrange = 0, tgtspeed = 0;
 	if (target) {
+		vector2 tgtxy = target->get_pos().xy();
+		vector2 plyxy = player->get_pos().xy();
+		vector2 delta = tgtxy - plyxy;
 		tgtcourse = target->get_heading().value();
-		tgtpos = (angle(target->get_pos().xy() - player->get_pos().xy())
-			  - player->get_heading()).value();
+		tgtpos = (angle(delta) - player->get_heading()).value();
+		tgtrange = delta.length();
+		tgtspeed = sea_object::ms2kts(target->get_speed());
 	}
+	s.targetcourse.draw(tgtcourse);
+	s.targetpos.draw(tgtpos);
+	if (tgtrange > 11000) tgtrange = 11000; // clamp display
+	s.targetrange.draw(tgtrange * 360 / 12000 + 15);
+	s.targetspeed.draw(15+tgtspeed*30/5);
 
-	// draw target position
-	if (is_day) {
-		targetpos_ptr_daylight->draw_rot(128, 188, tgtpos, 128-102, 188-109);
-	} else {
-		targetpos_ptr_nightlight->draw_rot(128, 188, tgtpos, 128-102, 188-109);
-	}
-
-	// faecherwinkel 339, 102, center 358, 188
-
-	// draw target course
-	if (is_day) {
-		targetcourse_ptr_daylight->draw_rot(586, 453, tgtcourse);
-	} else {
-		targetcourse_ptr_nightlight->draw_rot(586, 453, tgtcourse);
-	}
+	// spread angle
+	s.spreadangle.draw(77/*fixme*/);
 
 	// draw tubes if ready
-	unsigned tubex[6] = { 33,153,274,395,521,647};
-	unsigned tubey[6] = {618,618,618,618,618,618};
+	unsigned tubex[6] = { 31,150,271,392,518,641};
+	unsigned tubey[6] = {617,610,612,612,617,611};
 	for (unsigned i = 0; i < 6; ++i) {
 		if (player->is_tube_ready(i)) {
-			if (is_day) {
-				tube_textures_daylight[i]->draw(tubex[i], tubey[i]);
-			} else {
-				tube_textures_nightlight[i]->draw(tubex[i], tubey[i]);
-			}
+			s.tubelight[i]->draw(tubex[i], tubey[i]);
 		}
 	}
+
+	// tube turn switch
+	s.tubeswitch[selected_tube]->draw(760, 492);
+	if (player->is_tube_ready(selected_tube)) {
+		s.firebutton->draw(885, 354);
+	}
+
+	// automatic fire solution on / off switch
+	s.automode[selected_mode]->draw(713, 93);
 
 	sys().unprepare_2d_drawing();
 }
