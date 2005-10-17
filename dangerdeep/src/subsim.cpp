a35a629e448af3e0936fb25e9f038a6a8a0ea63c
Thorsten Jordan <thjordan@users.sf.net>
2005-10-17 18:52:02 +0000

    changes for win32 release

--- dangerdeep/src/subsim.cpp
+++ dangerdeep/src/subsim.cpp
@@ -1180,10 +1180,10 @@ int mymain(list<string>& args)
 	mycfg.register_option("fullscreen", true);
 	mycfg.register_option("debug", false);
 	mycfg.register_option("sound", true);
-	mycfg.register_option("water_res_x", 100);
+	mycfg.register_option("water_res_x", 99);
 	mycfg.register_option("water_res_y", 200);
 	mycfg.register_option("use_shaders", true);
-	mycfg.register_option("use_shaders_for_water", false);
+	mycfg.register_option("use_shaders_for_water", true);
 	mycfg.register_option("wave_fft_res", 128);
 	mycfg.register_option("wave_subdetail", true);
 	mycfg.register_option("wave_subdetail_size", 128);
