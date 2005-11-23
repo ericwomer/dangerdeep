b2fc247d0f9c1c324ce4ad97f98a8f01d59ba9dd
Thorsten Jordan <thjordan@users.sf.net>
2005-11-23 22:08:59 +0000

    changed (void) to ()

--- dangerdeep/src/angle.h
+++ dangerdeep/src/angle.h
@@ -30,14 +30,14 @@ class angle
 	angle() : val(0) {};
 	angle(double d) : val(d) {};
 	angle(const vector2& v) { val = (v == vector2(0,0)) ? 0 : 90-atan2(v.y, v.x)*180.0/M_PI; };
-	double value(void) const { return clamped(val); };
-	unsigned ui_value(void) const { return unsigned(clamped(round(val))); };
-	unsigned ui_abs_value180(void) const { return unsigned(fabs(round(value_pm180()))); };
-	double rad(void) const { return value()*M_PI/180.0; };
-	double value_pm180(void) const { double d = clamped(val); return d <= 180.0 ? d : d-360.0; };
+	double value() const { return clamped(val); };
+	unsigned ui_value() const { return unsigned(clamped(round(val))); };
+	unsigned ui_abs_value180() const { return unsigned(fabs(round(value_pm180()))); };
+	double rad() const { return value()*M_PI/180.0; };
+	double value_pm180() const { double d = clamped(val); return d <= 180.0 ? d : d-360.0; };
 	angle operator+(const angle& other) const { return angle(val + other.val); };
 	angle operator-(const angle& other) const { return angle(val - other.val); };
-	angle operator-(void) const { return angle(-val); };
+	angle operator-() const { return angle(-val); };
 	angle operator*(double t) const { return angle(val * t); };
 	bool is_cw_nearer(const angle& a) const { return clamped(a.val - val) <= 180.0; };
 	static angle from_rad(double d) { return angle(d*180.0/M_PI); };
@@ -45,9 +45,9 @@ class angle
 	angle& operator+=(const angle& other) { val += other.val; return *this; };
 	angle& operator-=(const angle& other) { val -= other.val; return *this; };
 	double diff(const angle& other) const { double d = clamped(other.val - val); if (d > 180.0) d = 360.0 - d; return d; };
-	double sin(void) const { return ::sin(rad()); };
-	double cos(void) const { return ::cos(rad()); };
-	vector2 direction(void) const { double r = rad(); return vector2(::sin(r), ::cos(r)); };
+	double sin() const { return ::sin(rad()); };
+	double cos() const { return ::cos(rad()); };
+	vector2 direction() const { double r = rad(); return vector2(::sin(r), ::cos(r)); };
 	bool operator == ( const angle& b ) const { return value() == b.value(); }
 	bool operator != ( const angle& b ) const { return value() != b.value(); }
 };
