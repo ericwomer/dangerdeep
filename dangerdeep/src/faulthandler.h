cecce6079642134a213730d8be3e4b9e1108d1c3
Thorsten Jordan <thjordan@users.sf.net>
2005-12-10 11:10:08 +0000

    faulthandler improved, added needed linker option

--- dangerdeep/src/faulthandler.h
+++ dangerdeep/src/faulthandler.h
@@ -9,30 +9,90 @@
 #include <execinfo.h>
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <cxxabi.h>      // Needed for __cxa_demangle
 #include <signal.h>
+#include <string>
+#include <sstream>
 
-void print_stack_trace()
+// Note: use --export-dynamic as linker option or you won't get function names here.
+
+inline void print_stack_trace()
 {
-	void *array[10];
-	size_t size = backtrace(array, 10);
+	void *array[16];
+	int size = backtrace(array, 16);
+	if (size < 0) {
+		fprintf(stderr, "Backtrace could not be created!\n");
+		return;
+	}
 	char** strings = backtrace_symbols (array, size);
-
-	printf("Stack trace: (%u frames)\n", size);
-	for (unsigned i = 0; i < size; ++i) {
-		printf("[%2d] %s\n", i, strings[i]);
+	if (!strings) {
+		fprintf(stderr, "Could not get Backtrace symbols!\n");
+		return;
 	}
 
+	fprintf(stderr, "Stack trace: (%u frames)\n", size);
+	std::string addrs;
+	std::list<std::string> lines;
+	for (unsigned i = 0; i < size; ++i) {
+		std::string addr;
+		std::string s(strings[i]);
+		unsigned p1 = s.rfind('[');
+		unsigned p2 = s.rfind(']');
+		if ((p1 != std::string::npos) && (p2 != std::string::npos)) {
+			addr = s.substr(p1 + 1, p2 - p1 - 1);
+			addrs += addr + " ";
+		}
+		p1 = s.rfind("_Z");
+		p2 = s.rfind('+');
+		if (p2 == std::string::npos)
+			p2 = s.rfind(')');
+		std::string func;
+		if (p1 != std::string::npos) {
+			func = s.substr(p1, p2 - p1);
+			int status = 0;
+			char* c = abi::__cxa_demangle(func.c_str(), 0, 0, &status);
+			if (c)
+				func = c;
+			else
+				func = "???";
+		} else {
+			p1 = s.rfind('(');
+			if (p1 != std::string::npos) {
+				func = s.substr(p1 + 1, p2 - p1 - 1);
+			} else {
+				p2 = s.rfind('[');
+				func = s.substr(0, p2 - 1);
+			}
+		}
+		lines.push_back(addr + " in " + func);
+		if (func == "main")
+			break;
+	}
 	free (strings);
+
+	// determine lines from addresses
+	std::ostringstream oss;
+	oss << "addr2line -e /proc/" << getpid() << "/exe -s " << addrs;
+	FILE* f = popen(oss.str().c_str(), "r");
+	if (f) {
+		for (std::list<std::string>::iterator it = lines.begin(); it != lines.end(); ++it) {
+			char tmp[128];
+			fgets(tmp, 127, f);
+			fprintf(stderr, "%s at %s", it->c_str(), tmp);
+		}
+		pclose(f);
+	} else {
+		for (std::list<std::string>::iterator it = lines.begin(); it != lines.end(); ++it) {
+			fprintf(stderr, "%s\n", it->c_str());
+		}
+	}
 }
-
-
 
 void sigsegv_handler(int )
 {
-	printf("SIGSEGV caught!\n");
+	fprintf(stderr, "SIGSEGV caught!\n");
 	print_stack_trace();
-	printf("Aborting program.\n");
+	fprintf(stderr, "Aborting program.\n");
 	abort();
 }
 
