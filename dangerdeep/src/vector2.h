44a9cb4a46ec009c04dcaebf5e243a03be474ef1
Thorsten Jordan <thjordan@users.sf.net>
2006-01-07 13:41:17 +0000

    added GPL copyright header

--- dangerdeep/src/vector2.h
+++ dangerdeep/src/vector2.h
@@ -1,3 +1,41 @@
+/*
+Danger from the Deep - Open source submarine simulation
+Copyright (C) 2003-2006  Thorsten Jordan, Luis Barrancos and others.
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+/*
+Danger from the Deep - Open source submarine simulation
+Copyright (C) 2003-2006  Thorsten Jordan, Luis Barrancos and others.
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
 //
 //  A 2d vector (C)+(W) 2001 Thorsten Jordan
 //
@@ -16,6 +54,7 @@
 
 template <class D2> class vector3t;
 
+/** Template class for a mathematical vector with two coefficients. */
 template <class D>
 class vector2t
 {
