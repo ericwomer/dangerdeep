c30b8d7a624b9bc877435f2cb22b3ec513a80456
Thorsten Jordan <thjordan@users.sf.net>
2006-12-25 13:14:11 +0000

    first version of GLSL wrapper classes

---
+++ dangerdeep/src/shader.cpp
@@ -0,0 +1,231 @@
+/*
+Danger from the Deep - Open source submarine simulation
+Copyright (C) 2003-2006  Thorsten Jordan, Luis Barrancos and others.
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+// OpenGL GLSL shaders
+// (C)+(W) by Thorsten Jordan. See LICENSE
+
+#include "shader.h"
+#include "oglext/OglExt.h"
+#include "system.h"
+#include "error.h"
+#include <stdexcept>
+#include <fstream>
+using namespace std;
+
+
+int glsl_program::glsl_supported = -1;
+bool glsl_program::supported()
+{
+	if (glsl_supported < 0) {
+		glsl_supported = (sys().extension_supported("GL_ARB_fragment_shader") &&
+				  sys().extension_supported("GL_ARB_shader_objects") &&
+				  /* sys().extension_supported("GL_ARB_shading_language_100") && */
+				  sys().extension_supported("GL_ARB_vertex_shader")) ? 1 : 0;
+	}
+	return glsl_supported == 1;
+}
+
+
+
+glsl_shader::glsl_shader(const string& filename, type stype)
+	: id(0)
+{
+	if (!glsl_program::supported())
+		throw std::runtime_error("GLSL shaders are not supported!");
+	switch (stype) {
+	case VERTEX:
+		id = glCreateShader(GL_VERTEX_SHADER);
+		break;
+	case FRAGMENT:
+		id = glCreateShader(GL_FRAGMENT_SHADER);
+		break;
+	default:
+		throw invalid_argument("invalid shader type");
+	}
+	if (id == 0)
+		throw runtime_error("can't create glsl shader");
+	try {
+		// read shader source
+		ifstream ifprg(filename.c_str(), ios::in);
+		if (ifprg.fail())
+			throw file_read_error(filename);
+
+		// read lines.
+		vector<string> lines;
+		while (!ifprg.eof()) {
+			string s;
+			getline(ifprg, s);
+			lines.push_back(s);
+		}
+
+		// fixme: add functionality from texture.cpp here
+#if 0
+		// build shader node tree by parsing.
+		unsigned current_line = 0;
+		std::auto_ptr<shader_node_program> snp(new shader_node_program(lines, current_line));
+
+		// compile to destination program
+		vector<string> finalprogram;
+		snp->compile(finalprogram, defines, false);
+		snp.reset();
+
+		// debug output
+		//	for (unsigned i = 0; i < finalprogram.size(); ++i)
+		//		cout << "line " << i << ": '" << finalprogram[i] << "'\n";
+#endif
+
+		string prg;
+		for (unsigned i = 0; i < lines /*finalprogram*/.size(); ++i)
+			prg += lines /*finalprogram*/[i] + "\n";
+
+		const char* prg_cstr = prg.c_str();
+		glShaderSource(id, 1, &prg_cstr, 0);
+
+		glCompileShader(id);
+
+		GLint compiled = GL_FALSE;
+		glGetShaderiv(id, GL_COMPILE_STATUS, &compiled);
+
+		if (!compiled) {
+			sys().add_console("compiling failed, log:");
+			GLint maxlength = 0;
+			glGetShaderiv(id, GL_INFO_LOG_LENGTH, &maxlength);
+			string log(maxlength+1, ' ');
+			GLint length = 0;
+			glGetShaderInfoLog(id, maxlength, &length, &log[0]);
+			sys().add_console(log);
+			throw runtime_error(string("compiling of shader failed : ") + filename);
+		}
+	}
+	catch (exception& e) {
+		glDeleteShader(id);
+		throw;
+	}
+}
+
+
+
+glsl_shader::~glsl_shader()
+{
+	glDeleteShader(id);
+}
+
+
+
+glsl_program::glsl_program()
+	: id(0), linked(false)
+{
+	if (!supported())
+		throw std::runtime_error("GLSL programs are not supported!");
+	id = glCreateProgram();
+	if (id == 0)
+		throw runtime_error("can't create glsl program");
+}
+
+
+
+glsl_program::glsl_program(glsl_shader& shader1, glsl_shader& shader2)
+	: id(0), linked(false)
+{
+	if (!supported())
+		throw std::runtime_error("GLSL programs are not supported!");
+	id = glCreateProgram();
+	if (id == 0)
+		throw runtime_error("can't create glsl program");
+	try {
+		attach(shader1);
+		attach(shader2);
+		link();
+	}
+	catch (exception& e) {
+		glDeleteProgram(id);
+		throw;
+	}
+}
+
+
+
+glsl_program::~glsl_program()
+{
+	for (list<glsl_shader*>::iterator it = attached_shaders.begin(); it != attached_shaders.end(); ++it)
+		glDetachShader(id, (*it)->id);
+	glDeleteProgram(id);
+}
+
+
+
+void glsl_program::attach(glsl_shader& s)
+{
+	glAttachShader(id, s.id);
+	attached_shaders.push_front(&s);
+	linked = false;
+}
+
+
+
+void glsl_program::detach(glsl_shader& s)
+{
+	glDetachShader(id, s.id);
+	for (list<glsl_shader*>::iterator it = attached_shaders.begin(); it != attached_shaders.end(); )
+		if (*it == &s) {
+			glDetachShader(id, (*it)->id);
+			it = attached_shaders.erase(it);
+		} else {
+			++it;
+		}
+	linked = false;
+}
+
+
+
+void glsl_program::link()
+{
+	glLinkProgram(id);
+	GLint waslinked = GL_FALSE;
+	glGetProgramiv(id, GL_LINK_STATUS, &waslinked);
+
+	if (!waslinked) {
+		sys().add_console("linking failed, log:");
+		GLint maxlength = 0;
+		glGetProgramiv(id, GL_INFO_LOG_LENGTH, &maxlength);
+		string log(maxlength+1, ' ');
+		GLint length = 0;
+		glGetProgramInfoLog(id, maxlength, &length, &log[0]);
+		sys().add_console(log);
+		throw runtime_error("linking of program failed");
+	}
+
+	linked = true;
+}
+
+
+
+void glsl_program::use() const
+{
+	if (!linked)
+		throw runtime_error("glsl_program::use() : program not linked");
+	glUseProgram(id);
+}
+
+
+
+void glsl_program::use_fixed()
+{
+	glUseProgram(0);
+}
---
+++ dangerdeep/src/shader.h
@@ -0,0 +1,106 @@
+/*
+Danger from the Deep - Open source submarine simulation
+Copyright (C) 2003-2006  Thorsten Jordan, Luis Barrancos and others.
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+// OpenGL GLSL shaders
+// (C)+(W) by Thorsten Jordan. See LICENSE
+
+#ifndef SHADER_H
+#define SHADER_H
+
+#include <string>
+#include <list>
+
+/// this class handles an OpenGL GLSL Shader
+///@note needs OpenGL 2.0.
+///@note reference counting is done by OpenGL.
+///@note shaders can be deleted after they have been attached to a glsl_program.
+class glsl_shader
+{
+ public:
+	/// type of shader (vertex or fragment, later maybe geometry shader with GF8800+)
+	enum type {
+		VERTEX,
+		FRAGMENT
+	};
+
+	/// create a shader
+	glsl_shader(const std::string& filename, type stype);
+
+	/// destroy a shader
+	~glsl_shader();
+
+ protected:
+	friend class glsl_program;	// for id access
+
+	unsigned id;
+
+ private:
+	glsl_shader();
+	glsl_shader(const glsl_shader& );
+	glsl_shader& operator= (const glsl_shader& );
+};
+
+
+
+/// this class handles an OpenGL GLSL Program, that is a link unit of shaders.
+///@note needs OpenGL 2.0
+class glsl_program
+{
+ public:
+	/// create program
+	glsl_program();
+
+	/// create program with two shaders, normally used for vertex+fragment shader pair
+	///@note will link program already!
+	glsl_program(glsl_shader& shader1, glsl_shader& shader2);
+
+	/// destroy program
+	~glsl_program();
+
+	/// attach a shader
+	void attach(glsl_shader& s);
+
+	/// attach a shader
+	void detach(glsl_shader& s);
+
+	/// link program after all shaders are attached
+	void link();
+
+	/// use this program
+	///@note will link the program first, if not done yet
+	void use() const;
+
+	/// use fixed function pipeline instead of particular program
+	static void use_fixed();
+
+	/// check if GLSL is supported
+	static bool supported();
+
+ protected:
+	unsigned id;
+	bool linked;
+	static int glsl_supported;
+	std::list<glsl_shader*> attached_shaders;
+
+ private:
+	glsl_program(const glsl_program& );
+	glsl_program& operator= (const glsl_program& );
+};
+
+#endif
