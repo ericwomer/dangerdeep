293e49f0b8291d07e37bc0bc2468fafba8b2040f
Thorsten Jordan <thjordan@users.sf.net>
2008-05-31 21:01:27 +0000

    remove hack

--- dangerdeep/src/submarine.cpp
+++ dangerdeep/src/submarine.cpp
@@ -499,9 +499,9 @@ void submarine::simulate(double delta_time)
 	//const double kg_per_sec = 1000; // 100 liters per second can be flooded/blowed out
 	double s1 = position.z - dive_to;
 	double s2 = linear_momentum.z * mass_inv;
-	double s3 = (200000 - mass_flooded_tanks) * 0.01;
+//	double s3 = (200000 - mass_flooded_tanks) * 0.01;
 //	double err = mysgn(s1)*s1*s1 + mysgn(s2)*s2*s2*20.0 + mysgn(s3)*s3*s3*0.03;
-	double err = 217000 + (s1 + s2*20.0)*1000.0;
+	double err = (s1 + s2*20.0)*1000.0;
 //	if (err < 1e-3) {
 	if (err < mass_flooded_tanks) {
 		// we need to go up, so empty ballast tanks
