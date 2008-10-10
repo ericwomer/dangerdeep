52d03dddec86b034f3d59087f2ccdcd03044d926
Matthias Bady <mbady@users.sf.net>
2008-10-10 09:43:07 +0000

    remove glBegin cruft

--- dangerdeep/src/sky.h
+++ dangerdeep/src/sky.h
@@ -36,6 +36,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #include "stars.h"
 #include "shader.h"
 #include "vertexbufferobject.h"
+#include "primitives.h"
 
 class game;
 
