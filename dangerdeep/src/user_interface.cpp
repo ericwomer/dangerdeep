ce5e03a5ff98df1e265c30c5e7c5c808d4b193e4
Matthias Bady <mbady@users.sf.net>
2009-03-16 13:34:42 +0000

    moved to fixed terrain resolution

--- dangerdeep/src/user_interface.cpp
+++ dangerdeep/src/user_interface.cpp
@@ -277,9 +277,8 @@ user_interface::user_interface(game& gm) :
 	// and #levels = j_max+1
 	// so #levels = floor(log2(z_far / (2^n * sample_spacing))) + 1
 	const double z_far = 20000.0;
-	unsigned terrain_nr_levels = 1 + unsigned(floor(log2(z_far / (1 << cfg::instance().geti("terrain_detail"))
-							     / mygame->get_height_gen().get_sample_spacing())));
-	mygeoclipmap.reset(new geoclipmap(terrain_nr_levels, cfg::instance().geti("terrain_detail"), mygame->get_height_gen()));
+
+	mygeoclipmap.reset(new geoclipmap(TERRAIN_NR_LEVELS, TERRAIN_RESOLUTION_N, mygame->get_height_gen()));
 
 	add_loading_screen("user interface initialized");
 }
