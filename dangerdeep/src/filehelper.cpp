8f0e095280678813fc1cb50a14548539d1b3c010
Matthew Lawrence <tx2rx@users.sf.net>
2009-04-29 23:38:12 +0000

    Added missing include

--- dangerdeep/src/filehelper.cpp
+++ dangerdeep/src/filehelper.cpp
@@ -23,6 +23,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #include "filehelper.h"
 #include "error.h"
 #include <vector>
+#include <stdio.h>
 using namespace std;
 
 #ifdef WIN32
