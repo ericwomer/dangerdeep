1e9b8f3224eaa9b74d10f4d454ff9a5017a1c079
Matthew Lawrence <tx2rx@users.sf.net>
2009-04-29 23:37:22 +0000

    More non-gcc compiler fixes, we need time.h on non-win32 platforms as well

--- dangerdeep/src/subsim.cpp
+++ dangerdeep/src/subsim.cpp
@@ -23,8 +23,9 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #ifdef WIN32
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
-#include <time.h>
 #endif
+
+#include <time.h>
 
 #include "oglext/OglExt.h"
 #include <glu.h>
