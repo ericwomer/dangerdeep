4493937a747783e93d7ace4a783b173470261403
Matthew Lawrence <tx2rx@users.sf.net>
2010-03-06 13:14:06 +0000

    Remove direct gl.h include, use OglExt.h instead.

--- dangerdeep/src/dftdtester/tests.cpp
+++ dangerdeep/src/dftdtester/tests.cpp
@@ -29,7 +29,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #include <sstream>
 #include <set>
 #include <stdlib.h>
-#include <GL/gl.h>
+#include "../oglext/OglExt.h"
 
 #include "tests.h"
 
