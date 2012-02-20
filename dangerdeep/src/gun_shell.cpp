0be6c6c97916e7edfe0c688cabe617b7550e9c2f
Matthew Lawrence <tx2rx@users.sf.net>
2012-02-20 22:12:05 +0000

    Ensure escort ships do not get damaged by their own gun fire.

--- dangerdeep/src/gun_shell.cpp
+++ dangerdeep/src/gun_shell.cpp
@@ -51,9 +51,9 @@ gun_shell::gun_shell(game& gm_, const vector3& pos, angle direction, angle eleva
 	linear_momentum = mass * orientation.rotate(vector3(0,
 		elevation.cos() * initial_velocity,
 		elevation.sin() * initial_velocity));
-	// set off initial pos. like 0.1 seconds after firing, to avoid
+	// set off initial pos. like 0.5 seconds after firing, to avoid
 	// collision with parent
-	position = pos + linear_momentum * (mass_inv * 0.1);
+	position = pos + linear_momentum * (mass_inv * 0.5);
 	angular_momentum = vector3();
 	compute_helper_values();
 	oldpos = position;
